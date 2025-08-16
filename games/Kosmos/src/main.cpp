a4c72e416f91990cfc9f03c638f974d0013a4dc3
David Rowbotham <thedaver64@gmail.com>
2025-08-16 06:58:40 +0100

    minor fixes.

--- games/Kosmos/src/main.cpp
+++ games/Kosmos/src/main.cpp
@@ -1,4 +1,4 @@
-#define SDL_MAIN_HANDLED  // Prevent SDL from redefining main
+#define SDL_MAIN_HANDLED // Prevent SDL from redefining main
 #include "KosmosBase.h"
 #include "DotBlue/DotBlue.h"
 #include "DotBlue/GLPlatform.h"
@@ -32,10 +32,10 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 
 #ifdef _WIN32
 // Forward declaration of window message handler
-static long HandleWindowMessage(void* hwnd, unsigned int msg, unsigned long long wParam, long long lParam);
+static long HandleWindowMessage(void *hwnd, unsigned int msg, unsigned long long wParam, long long lParam);
 #elif defined(__linux__) || defined(__FreeBSD__)
 // Forward declaration of X11 event handler
-static void HandleX11Event(void* xevent);
+static void HandleX11Event(void *xevent);
 #endif
 
 #include <chrono>
@@ -53,38 +53,39 @@ public:
 
     bool Initialize() override
     {
-    std::cout << "Initializing Kosmos..." << std::endl;
+        std::cout << "Initializing Kosmos..." << std::endl;
 #ifdef _WIN32
-    DotBlue::SetWindowMessageCallback(HandleWindowMessage);
+        DotBlue::SetWindowMessageCallback(HandleWindowMessage);
 #elif defined(__linux__) || defined(__FreeBSD__)
-    DotBlue::SetX11EventCallback(HandleX11Event);
+        DotBlue::SetX11EventCallback(HandleX11Event);
 #endif
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    ImGui::StyleColorsDark();
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        ImGui::StyleColorsDark();
 #ifdef _WIN32
-    HWND hwnd = static_cast<HWND>(DotBlue::GetWindowHandle());
-    ImGui_ImplWin32_Init(hwnd);
+        HWND hwnd = static_cast<HWND>(DotBlue::GetWindowHandle());
+        ImGui_ImplWin32_Init(hwnd);
 #endif
-    ImGui_ImplOpenGL3_Init("#version 130");
-    showKosmosUI = true;
+        ImGui_ImplOpenGL3_Init("#version 130");
+        showKosmosUI = true;
 
-    // Configure camera for a basic 3D view
-    camera.setPosition(glm::dvec3(0.0, 0.0, 5.0));
-    camera.setTarget(glm::dvec3(0.0, 0.0, 0.0));
-    camera.setUp(glm::dvec3(0.0, 1.0, 0.0));
-    camera.setFOV(60.0);
-    camera.setAspect(16.0 / 9.0); // TODO: Query actual window size
-    camera.setNearFar(0.1, 1000.0);
+        // Configure camera for a basic 3D view
+        camera.setPosition(glm::dvec3(0.0, 0.0, 5.0));
+        camera.setTarget(glm::dvec3(0.0, 0.0, 0.0));
+        camera.setUp(glm::dvec3(0.0, 1.0, 0.0));
+        camera.setFOV(60.0);
+        camera.setAspect(16.0 / 9.0); // TODO: Query actual window size
+        camera.setNearFar(0.1, 1000.0);
 
-    return true;
+        return true;
     }
 
     void Update(float deltaTime) override
     {
-    // Future: camera movement, input, etc.
+        // Future: camera movement, input, etc.
     }
 
     void Render() override
@@ -103,7 +104,7 @@ public:
     void RenderUI()
     {
         int width = 800, height = 600;
-        ImGuiIO& io = ImGui::GetIO();
+        ImGuiIO &io = ImGui::GetIO();
         io.DisplaySize.x = (float)width;
         io.DisplaySize.y = (float)height;
 #ifdef _WIN32
@@ -112,7 +113,8 @@ public:
         ImGui_ImplOpenGL3_NewFrame();
         ImGui::NewFrame();
 
-        if (showKosmosUI) {
+        if (showKosmosUI)
+        {
             ImGui::Begin("Kosmos UI", &showKosmosUI);
             ImGui::Text("3D camera configured. Ready for scene rendering.");
             ImGui::End();
@@ -122,139 +124,200 @@ public:
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
 
-    void HandleInput(const DotBlue::InputManager& input, const DotBlue::InputBindings& bindings) override
+    void HandleInput(const DotBlue::InputManager &input, const DotBlue::InputBindings &bindings) override
     {
-    // Implement camera controls/input here in the future
+        // Implement camera controls/input here in the future
     }
 
     void Shutdown() override
     {
-    std::cout << "[Kosmos] Shutdown() called." << std::endl; std::cout.flush();
-    ImGui_ImplOpenGL3_Shutdown();
+        std::cout << "[Kosmos] Shutdown() called." << std::endl;
+        std::cout.flush();
+        ImGui_ImplOpenGL3_Shutdown();
 #ifdef _WIN32
-    ImGui_ImplWin32_Shutdown();
+        ImGui_ImplWin32_Shutdown();
 #endif
-    ImGui::DestroyContext();
-    std::cout << "[Kosmos] Shutdown() completed." << std::endl; std::cout.flush();
+        ImGui::DestroyContext();
+        std::cout << "[Kosmos] Shutdown() completed." << std::endl;
+        std::cout.flush();
     }
 };
 
 #ifdef _WIN32
 // Window message handler for ImGui input
-static long HandleWindowMessage(void* hwnd, unsigned int msg, unsigned long long wParam, long long lParam)
+static long HandleWindowMessage(void *hwnd, unsigned int msg, unsigned long long wParam, long long lParam)
 {
     // Cast to proper Windows types
     HWND hWnd = static_cast<HWND>(hwnd);
     UINT uMsg = static_cast<UINT>(msg);
     WPARAM wp = static_cast<WPARAM>(wParam);
     LPARAM lp = static_cast<LPARAM>(lParam);
-    
+
     // Let ImGui handle the message
     LRESULT result = ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wp, lp);
-    
+
     // Return the result directly as a long
     return static_cast<long>(result);
 }
 #elif defined(__linux__) || defined(__FreeBSD__)
 // X11 event handler for ImGui input
-static void HandleX11Event(void* xevent)
+static void HandleX11Event(void *xevent)
 {
-    XEvent* xev = static_cast<XEvent*>(xevent);
-    ImGuiIO& io = ImGui::GetIO();
-    
+    XEvent *xev = static_cast<XEvent *>(xevent);
+    ImGuiIO &io = ImGui::GetIO();
+
     switch (xev->type)
     {
-        case MotionNotify:
-            io.MousePos = ImVec2((float)xev->xmotion.x, (float)xev->xmotion.y);
-            break;
-            
-        case ButtonPress:
-            if (xev->xbutton.button == Button1) io.MouseDown[0] = true;  // Left mouse
-            if (xev->xbutton.button == Button3) io.MouseDown[1] = true;  // Right mouse
-            if (xev->xbutton.button == Button2) io.MouseDown[2] = true;  // Middle mouse
-            break;
-            
-        case ButtonRelease:
-            if (xev->xbutton.button == Button1) io.MouseDown[0] = false;
-            if (xev->xbutton.button == Button3) io.MouseDown[1] = false;
-            if (xev->xbutton.button == Button2) io.MouseDown[2] = false;
-            break;
-            
-        case KeyPress:
+    case MotionNotify:
+        io.MousePos = ImVec2((float)xev->xmotion.x, (float)xev->xmotion.y);
+        break;
+
+    case ButtonPress:
+        if (xev->xbutton.button == Button1)
+            io.MouseDown[0] = true; // Left mouse
+        if (xev->xbutton.button == Button3)
+            io.MouseDown[1] = true; // Right mouse
+        if (xev->xbutton.button == Button2)
+            io.MouseDown[2] = true; // Middle mouse
+        break;
+
+    case ButtonRelease:
+        if (xev->xbutton.button == Button1)
+            io.MouseDown[0] = false;
+        if (xev->xbutton.button == Button3)
+            io.MouseDown[1] = false;
+        if (xev->xbutton.button == Button2)
+            io.MouseDown[2] = false;
+        break;
+
+    case KeyPress:
+    {
+        KeySym keysym = XLookupKeysym(&xev->xkey, 0);
+
+        // Map X11 keys to ImGui keys (ImGui 1.90+ modern API)
+        ImGuiKey imgui_key = ImGuiKey_None;
+        if (keysym >= XK_a && keysym <= XK_z)
         {
-            KeySym keysym = XLookupKeysym(&xev->xkey, 0);
-            
-            // Map X11 keys to ImGui keys (ImGui 1.90+ modern API)
-            ImGuiKey imgui_key = ImGuiKey_None;
-            if (keysym >= XK_a && keysym <= XK_z) {
-                imgui_key = (ImGuiKey)(ImGuiKey_A + (keysym - XK_a));
-            } else if (keysym >= XK_0 && keysym <= XK_9) {
-                imgui_key = (ImGuiKey)(ImGuiKey_0 + (keysym - XK_0));
-            } else {
-                // Map some common special keys
-                switch (keysym) {
-                    case XK_space: imgui_key = ImGuiKey_Space; break;
-                    case XK_Return: imgui_key = ImGuiKey_Enter; break;
-                    case XK_Escape: imgui_key = ImGuiKey_Escape; break;
-                    case XK_BackSpace: imgui_key = ImGuiKey_Backspace; break;
-                    case XK_Tab: imgui_key = ImGuiKey_Tab; break;
-                    case XK_Left: imgui_key = ImGuiKey_LeftArrow; break;
-                    case XK_Right: imgui_key = ImGuiKey_RightArrow; break;
-                    case XK_Up: imgui_key = ImGuiKey_UpArrow; break;
-                    case XK_Down: imgui_key = ImGuiKey_DownArrow; break;
-                    default: break;
-                }
-            }
-            
-            if (imgui_key != ImGuiKey_None) {
-                io.AddKeyEvent(imgui_key, true);
-            }
-            
-            // Handle character input for printable characters
-            if (keysym >= 32 && keysym <= 126) {
-                io.AddInputCharacter((unsigned int)keysym);
-            }
-            break;
+            imgui_key = (ImGuiKey)(ImGuiKey_A + (keysym - XK_a));
         }
-        
-        case KeyRelease:
+        else if (keysym >= XK_0 && keysym <= XK_9)
         {
-            KeySym keysym = XLookupKeysym(&xev->xkey, 0);
-            
-            // Map X11 keys to ImGui keys (same mapping as KeyPress)
-            ImGuiKey imgui_key = ImGuiKey_None;
-            if (keysym >= XK_a && keysym <= XK_z) {
-                imgui_key = (ImGuiKey)(ImGuiKey_A + (keysym - XK_a));
-            } else if (keysym >= XK_0 && keysym <= XK_9) {
-                imgui_key = (ImGuiKey)(ImGuiKey_0 + (keysym - XK_0));
-            } else {
-                switch (keysym) {
-                    case XK_space: imgui_key = ImGuiKey_Space; break;
-                    case XK_Return: imgui_key = ImGuiKey_Enter; break;
-                    case XK_Escape: imgui_key = ImGuiKey_Escape; break;
-                    case XK_BackSpace: imgui_key = ImGuiKey_Backspace; break;
-                    case XK_Tab: imgui_key = ImGuiKey_Tab; break;
-                    case XK_Left: imgui_key = ImGuiKey_LeftArrow; break;
-                    case XK_Right: imgui_key = ImGuiKey_RightArrow; break;
-                    case XK_Up: imgui_key = ImGuiKey_UpArrow; break;
-                    case XK_Down: imgui_key = ImGuiKey_DownArrow; break;
-                    default: break;
-                }
-            }
-            
-            if (imgui_key != ImGuiKey_None) {
-                io.AddKeyEvent(imgui_key, false);
-            }
-            break;
+            imgui_key = (ImGuiKey)(ImGuiKey_0 + (keysym - XK_0));
         }
-        
-        case FocusIn:
-            io.AddFocusEvent(true);
-            break;
-            
-        case FocusOut:
-            io.AddFocusEvent(false);
-            break;
+        else
+        {
+            // Map some common special keys
+            switch (keysym)
+            {
+            case XK_space:
+                imgui_key = ImGuiKey_Space;
+                break;
+            case XK_Return:
+                imgui_key = ImGuiKey_Enter;
+                break;
+            case XK_Escape:
+                imgui_key = ImGuiKey_Escape;
+                break;
+            case XK_BackSpace:
+                imgui_key = ImGuiKey_Backspace;
+                break;
+            case XK_Tab:
+                imgui_key = ImGuiKey_Tab;
+                break;
+            case XK_Left:
+                imgui_key = ImGuiKey_LeftArrow;
+                break;
+            case XK_Right:
+                imgui_key = ImGuiKey_RightArrow;
+                break;
+            case XK_Up:
+                imgui_key = ImGuiKey_UpArrow;
+                break;
+            case XK_Down:
+                imgui_key = ImGuiKey_DownArrow;
+                break;
+            default:
+                break;
+            }
+        }
+
+        if (imgui_key != ImGuiKey_None)
+        {
+            io.AddKeyEvent(imgui_key, true);
+        }
+
+        // Handle character input for printable characters
+        if (keysym >= 32 && keysym <= 126)
+        {
+            io.AddInputCharacter((unsigned int)keysym);
+        }
+        break;
+    }
+
+    case KeyRelease:
+    {
+        KeySym keysym = XLookupKeysym(&xev->xkey, 0);
+
+        // Map X11 keys to ImGui keys (same mapping as KeyPress)
+        ImGuiKey imgui_key = ImGuiKey_None;
+        if (keysym >= XK_a && keysym <= XK_z)
+        {
+            imgui_key = (ImGuiKey)(ImGuiKey_A + (keysym - XK_a));
+        }
+        else if (keysym >= XK_0 && keysym <= XK_9)
+        {
+            imgui_key = (ImGuiKey)(ImGuiKey_0 + (keysym - XK_0));
+        }
+        else
+        {
+            switch (keysym)
+            {
+            case XK_space:
+                imgui_key = ImGuiKey_Space;
+                break;
+            case XK_Return:
+                imgui_key = ImGuiKey_Enter;
+                break;
+            case XK_Escape:
+                imgui_key = ImGuiKey_Escape;
+                break;
+            case XK_BackSpace:
+                imgui_key = ImGuiKey_Backspace;
+                break;
+            case XK_Tab:
+                imgui_key = ImGuiKey_Tab;
+                break;
+            case XK_Left:
+                imgui_key = ImGuiKey_LeftArrow;
+                break;
+            case XK_Right:
+                imgui_key = ImGuiKey_RightArrow;
+                break;
+            case XK_Up:
+                imgui_key = ImGuiKey_UpArrow;
+                break;
+            case XK_Down:
+                imgui_key = ImGuiKey_DownArrow;
+                break;
+            default:
+                break;
+            }
+        }
+
+        if (imgui_key != ImGuiKey_None)
+        {
+            io.AddKeyEvent(imgui_key, false);
+        }
+        break;
+    }
+
+    case FocusIn:
+        io.AddFocusEvent(true);
+        break;
+
+    case FocusOut:
+        io.AddFocusEvent(false);
+        break;
     }
 }
 #endif
